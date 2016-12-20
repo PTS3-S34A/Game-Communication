418b2f74db5d451485b1385f08419629ca45e30e
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2016-12-20 12:11:15 +0100

    Added new message ids.

--- GameServer/src/nl/soccar/socnet/message/MessageConstants.java
+++ GameServer/src/nl/soccar/socnet/message/MessageConstants.java
@@ -14,16 +14,19 @@ public final class MessageConstants {
     public static final int PLAYER_JOINED_SESSION_MESSAGE_ID = 3;
     public static final int PLAYER_LEFT_SESSION_MESSAGE_ID = 4;
     public static final int PLAYER_LEAVE_SESSION_MESSAGE_ID = 5;
-    public static final int START_GAME_MESSAGE_ID = 6;
-    public static final int PLAYER_STARTED_GAME_MESSAGE_ID = 7;
+    public static final int CHANGE_GAME_STATUS_MESSAGE_ID = 6;
+    public static final int PLAYER_CHANGED_GAME_STATUS_MESSAGE_ID = 7;
     public static final int PLAYER_MOVED_MESSAGE_ID = 8;
     public static final int MOVE_PLAYER_MESSAGE_ID = 9;
     public static final int SYNC_POSITION_PLAYER_MESSAGE_ID = 10;
     public static final int CHAT_MESSAGE_ID = 11;
     public static final int SWITCH_TEAM_MESSAGE_ID = 12;
     public static final int SYNC_POSITION_BALL_MESSAGE_ID = 13;
-    public static final int SPAWN_ENTITY_MESSAGE_ID = 14;
-    
+    public static final int SPAWN_CAR_MESSAGE_ID = 14;
+    public static final int CHANGE_PLAYER_STATUS_MESSAGE_ID = 15;
+    public static final int SPAWN_OBSTACLE_MESSAGE_ID = 16;
+    public static final int SPAWN_BALL_MESSAGE_ID = 17;
+
     private MessageConstants() {
     }
     
